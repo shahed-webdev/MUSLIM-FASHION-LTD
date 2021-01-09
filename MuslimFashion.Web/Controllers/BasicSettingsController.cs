1c8dbaede25f333f737fa831740dbb65861fcab7
muhebollah <muhebollah.diu@gamil.com>
2021-01-09 16:57:48 +0600

    add setting page

---
+++ MuslimFashion.Web/Controllers/BasicSettingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuslimFashion.Web.Controllers
+{
+    public class BasicSettingsController : Controller
+    {
+        public IActionResult ImageSlider()
+        {
+            return View();
+        }
+        
+        //brand
+        public IActionResult Brands()
+        {
+            return View();
+        }
+    }
+}
