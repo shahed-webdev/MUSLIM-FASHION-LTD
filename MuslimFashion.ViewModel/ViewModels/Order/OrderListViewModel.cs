8173dd55a58c8c381ad01107d5f8d39b78896074
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2021-04-10 11:05:59 +0600

    date nullable OrderListViewModel

--- MuslimFashion.ViewModel/ViewModels/Order/OrderListViewModel.cs
+++ MuslimFashion.ViewModel/ViewModels/Order/OrderListViewModel.cs
@@ -10,7 +10,7 @@ namespace MuslimFashion.ViewModel
         public string OrderStatus { get; set; }
         public decimal NetAmount { get; set; }
         public DateTime OrderDate { get; set; }
-        public DateTime StatusChangeDate { get; set; }
+        public DateTime? StatusChangeDate { get; set; }
         public string DeliveryName { get; set; }
         public string DeliveryAddress { get; set; }
         public string DeliveryPhone { get; set; }
